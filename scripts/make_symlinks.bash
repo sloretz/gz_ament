03938d3592458341d94a4bede4c801c800b3c231
Chris Lalancette <clalancette@openrobotics.org>
2017-07-10 14:58:23 -0400

    Make the names of the package files and directories match.
    This will make the scripts easier.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- scripts/make_symlinks.bash
+++ scripts/make_symlinks.bash
@@ -6,28 +6,28 @@ SRC_DIR=$DIR/../src
 PKG_DIR=../package.xml
 
 cd $SRC_DIR/gazebo_experimental
-ln -Ts ../$PKG_DIR/gazebo_experimental.xml package.xml || true
+ln -fTs ../$PKG_DIR/gazebo_experimental.xml package.xml || true
 
 cd $SRC_DIR/sdformat
-ln -Ts ../$PKG_DIR/sdformat.xml package.xml || true
+ln -fTs ../$PKG_DIR/sdformat.xml package.xml || true
 
-cd $SRC_DIR/ign/tools
-ln -Ts ../../$PKG_DIR/ign_tools.xml package.xml || true
+cd $SRC_DIR/ign-tools
+ln -fTs ../$PKG_DIR/ign-tools.xml package.xml || true
 
-cd $SRC_DIR/ign/common
-ln -Ts ../../$PKG_DIR/ign_common.xml package.xml || true
+cd $SRC_DIR/ign-common
+ln -fTs ../$PKG_DIR/ign-common.xml package.xml || true
 
-cd $SRC_DIR/ign/gui
-ln -Ts ../../$PKG_DIR/ign_gui.xml package.xml || true
+cd $SRC_DIR/ign-gui
+ln -fTs ../$PKG_DIR/ign-gui.xml package.xml || true
 
-cd $SRC_DIR/ign/msgs
-ln -Ts ../../$PKG_DIR/ign_msgs.xml package.xml || true
+cd $SRC_DIR/ign-msgs
+ln -fTs ../$PKG_DIR/ign-msgs.xml package.xml || true
 
-cd $SRC_DIR/ign/rendering
-ln -Ts ../../$PKG_DIR/ign_rendering.xml package.xml || true
+cd $SRC_DIR/ign-rendering
+ln -fTs ../$PKG_DIR/ign-rendering.xml package.xml || true
 
-cd $SRC_DIR/ign/transport
-ln -Ts ../../$PKG_DIR/ign_transport.xml package.xml || true
+cd $SRC_DIR/ign-transport
+ln -fTs ../$PKG_DIR/ign-transport.xml package.xml || true
 
-cd $SRC_DIR/ign/math
-ln -Ts ../../$PKG_DIR/ign_math.xml package.xml || true
+cd $SRC_DIR/ign-math
+ln -fTs ../$PKG_DIR/ign-math.xml package.xml || true
