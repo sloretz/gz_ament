67784424c9d0db54a33be6680d83dd4c344c6686
Chris Lalancette <clalancette@openrobotics.org>
2017-07-10 15:03:01 -0400

    Rewrite make_symlinks to be a loop.
    It's much less duplication of code this way.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- scripts/make_symlinks.bash
+++ scripts/make_symlinks.bash
@@ -5,29 +5,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SRC_DIR=$DIR/../src
 PKG_DIR=../package.xml
 
-cd $SRC_DIR/gazebo_experimental
-ln -fTs ../$PKG_DIR/gazebo_experimental.xml package.xml || true
+SRC_LIST="gazebo_experimental sdformat ign-tools ign-common ign-gui ign-msgs ign-rendering ign-transport ign-math"
 
-cd $SRC_DIR/sdformat
-ln -fTs ../$PKG_DIR/sdformat.xml package.xml || true
-
-cd $SRC_DIR/ign-tools
-ln -fTs ../$PKG_DIR/ign-tools.xml package.xml || true
-
-cd $SRC_DIR/ign-common
-ln -fTs ../$PKG_DIR/ign-common.xml package.xml || true
-
-cd $SRC_DIR/ign-gui
-ln -fTs ../$PKG_DIR/ign-gui.xml package.xml || true
-
-cd $SRC_DIR/ign-msgs
-ln -fTs ../$PKG_DIR/ign-msgs.xml package.xml || true
-
-cd $SRC_DIR/ign-rendering
-ln -fTs ../$PKG_DIR/ign-rendering.xml package.xml || true
-
-cd $SRC_DIR/ign-transport
-ln -fTs ../$PKG_DIR/ign-transport.xml package.xml || true
-
-cd $SRC_DIR/ign-math
-ln -fTs ../$PKG_DIR/ign-math.xml package.xml || true
+for src in $SRC_LIST; do
+	pushd "$SRC_DIR/$src" >& /dev/null
+	ln -fTs ../$PKG_DIR/$src.xml package.xml || true
+	popd >& /dev/null
+done
