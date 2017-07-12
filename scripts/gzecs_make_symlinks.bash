ab261999fbe49c634d35085d02c8749035c950eb
Chris Lalancette <clalancette@gmail.com>
2017-07-12 07:26:49 -0700

    Add the ability to build regular gazebo (#1)
    * Switch to using https URLs.
    
    This make the repos file more generically useful.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * Add in ignition-math as a dependency.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * Make ignition-msgs depend on ign-math and ign-tools.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * Move gazebo.xml to gazebo_experimental.xml
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * Make the names of the package files and directories match.
    
    This will make the scripts easier.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * Rewrite make_symlinks to be a loop.
    
    It's much less duplication of code this way.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * Rename make_symlinks.bash to gzecs_make_symlinks.bash.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * Sort everything in alphabetical order.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * Add in scripting to be able to build gazebo9.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

---
+++ scripts/gzecs_make_symlinks.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+SRC_DIR=$DIR/../src
+PKG_DIR=../package.xml
+
+SRC_LIST="gazebo_experimental ign-common ign-gui ign-math ign-msgs ign-rendering ign-tools ign-transport sdformat"
+
+for src in $SRC_LIST; do
+	pushd "$SRC_DIR/$src" >& /dev/null
+	ln -fTs ../$PKG_DIR/$src.xml package.xml || true
+	popd >& /dev/null
+done
