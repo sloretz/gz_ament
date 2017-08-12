777d475053e69c450c20413a14560a0ee36664ae
Shane Loretz <sloretz@osrfoundation.org>
2017-08-11 17:11:09 -0700

    Added ign-sensors. Changed ign-math branch to default

--- scripts/gzecs_make_symlinks.bash
+++ scripts/gzecs_make_symlinks.bash
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SRC_DIR=$DIR/../src
 PKG_DIR=../package.xml
 
-SRC_LIST="gazebo_experimental ign-common ign-gui ign-math ign-msgs ign-rendering ign-tools ign-transport sdformat"
+SRC_LIST="gazebo_experimental ign-common ign-gui ign-math ign-msgs ign-rendering ign-sensors ign-tools ign-transport sdformat"
 
 for src in $SRC_LIST; do
 	pushd "$SRC_DIR/$src" >& /dev/null
